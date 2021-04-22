81c3c1d529c993d26c54473aafe4086fd51bcbf9
takumi20210430 <uniquetime.t@gmail.com>
2021-04-22 12:19:57 +0000

    [Finish]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0e7aa59cd2ddf9224b027550993723cf8f90c33da87af000344f1ca68633a317715bf4c514e7b3ea035550aa1aaa297a3759198eac3d131c7101caeb83774543'
